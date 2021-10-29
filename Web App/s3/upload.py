2304a52e9d114e6a20f97f2d7150b1355e73218c
unknown <aidenpearcewd01@gmail.com>
2021-10-29 18:45:39 +0700

    's3'

--- Web App/s3/upload.py
+++ Web App/s3/upload.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile, Form
+import os
 import shutil
 import boto3
 
@@ -12,6 +13,9 @@ bucket = s3.Bucket('final-web-usth')
 def _file_upload(file: UploadFile = File(...)):
     with open(file.filename, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer) # save on server 
-        bucket.upload_fileobj(file.file,"cat.jpg") #upload to S3
+
+    with open(file.filename, "rb") as data:
+        bucket.upload_fileobj(data,file.filename) #upload to S3
+        os.remove(file.filename)
 
     return {"filename": file.filename}
