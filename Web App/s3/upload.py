2db8daef48bfffe7c5a369e86f40532525054866
unknown <aidenpearcewd01@gmail.com>
2021-10-29 19:05:45 +0700

    's3-update'

--- Web App/s3/upload.py
+++ Web App/s3/upload.py
@@ -8,6 +8,7 @@ app = FastAPI()
 #s3 Configuration
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('final-web-usth')
+location = boto3.client('s3').get_bucket_location(Bucket='final-web-usth')['LocationConstraint']
 
 @app.post('/file')
 def _file_upload(file: UploadFile = File(...)):
@@ -18,4 +19,5 @@ def _file_upload(file: UploadFile = File(...)):
         bucket.upload_fileobj(data,file.filename) #upload to S3
         os.remove(file.filename)
 
-    return {"filename": file.filename}
+    return {"filename": "https://s3-%s.amazonaws.com/%s/%s" % (location, 'final-web-usth', file.filename)}
+
